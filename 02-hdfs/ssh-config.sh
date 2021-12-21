7da6a7ba1e29ab5fedd74b8f49bcfee0745055fe
WeslyG Allan <weslyg22@gmail.com>
2021-12-21 23:13:49 +0500

    fix2

--- 02-hdfs/ssh-config.sh
+++ 02-hdfs/ssh-config.sh
@@ -5,7 +5,7 @@ source conf/config.sh
 
 echo $HADOOP_USER_PASSWORD | sudo -S bash -c 'cat conf/hosts >> /etc/hosts'
 
-ssh-keygen -t rsa ~/.ssh/id_rsa # generate ssh key for the node
+ssh-keygen -t rsa -f ~/.ssh/id_rsa # generate ssh key for the node
 
 #HOSTNAMES=`awk '{print $2}' conf/hosts` # get all hostnames in conf/hosts file
 
